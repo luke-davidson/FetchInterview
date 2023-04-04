e2ede2f7bd65d0bab1bedb7b6fa7696bc5600a84
Luke Davidson <davidsonab6@gmail.com>
2023-04-03 21:30:24 -0400

    updates

--- main.py
+++ main.py
@@ -5,13 +5,25 @@ import os
 import matplotlib.pyplot as plt
 from flask import Flask, render_template, request
 
+# Import and create model
+from model import Modeler
+model = Modeler()
+model()
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("template.html")
 
-
+@app.route("/inputmonth", methods=["GET", "POST"])
+def inputmonth():
+    if request.method == "GET":
+        return render_template("month.html")
+    elif request.method == "POST":
+        month_num = int(request.form.get("month"))
+        pred = model.predict(month_num)
+        return render_template("output.html", pred=pred)
 
 
 
